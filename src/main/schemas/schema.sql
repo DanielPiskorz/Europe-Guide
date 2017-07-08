94f6d5434facf2de799b49729d4156daa5fcc5e4
DanielPiskorz <eel5426@gmail.com>
2017-07-08 22:37:09 +0200

    Adds missing countries to SQL script.

--- src/main/schemas/schema.sql
+++ src/main/schemas/schema.sql
@@ -9,40 +9,58 @@ INSERT INTO country
 VALUES
 	('Albania', 'Tirana'),
 	('Andorra', 'Andorra la Vella'),
+	('Armenia', 'Yerevan'),
 	('Austria', 'Vienna'),
+	('Azerbaijan', 'Baku'),
+
 	('Belarus', 'Minsk'),
 	('Belgium', 'Brussels'),
 	('Bosnia and Herzegovina', 'Sarajevo'),
 	('Bulgaria', 'Sofia'),
+
 	('Croatia', 'Zagreb'),
 	('Cyprus', 'Nicosia'),
 	('Czech Republic', 'Prague'),
+
 	('Denmark', 'Copenagen'),
+
 	('Estonia', 'Tallinn'),
+
 	('Finland', 'Helsinki'),
 	('France', 'Paris'),
+
+	('Georgia', 'Tbilisi'),
 	('Germany', 'Berlin'),
 	('Greece', 'Athens'),
+
 	('Hungary', 'Budapest'),
+
 	('Iceland', 'Reykjaík'),
 	('Ireland', 'Dublin'),
 	('Italy', 'Italian'),
+
 	('Kosovo', 'Pristina'),
+
 	('Latvia', 'Riga'),
 	('Liechtenstein', 'Vaduz'),
 	('Lithuania', 'Vilnius'),
 	('Luxembourg', 'Luxembourg'),
+
 	('Macedonia', 'Skopje'),
 	('Malta', 'Valletta'),
 	('Moldova', 'Chisinau'),
 	('Monaco', 'Monaco'),
 	('Montenegro', 'Podgorica'),
+
 	('Netherlands', 'Amsterdam'),
 	('Norway', 'Oslo'),
+
 	('Poland', 'Warsaw'),
 	('Portugal', 'Lisbon'),
+
 	('Romania', 'Bucharest'),
 	('Russia', 'Moscow'),
+
 	('San Marino', 'San Marino'),
 	('Serbia', 'Belgrade'),
 	('Slovakia', 'Bratislava'),
@@ -50,6 +68,10 @@ VALUES
 	('Spain', 'Madrid'),
 	('Sweden', 'Stockholm'),
 	('Switzerland', 'Bern'),
+
+	('Turkey', 'Ankara'),
+
 	('Ukraine', 'Kiev'),
 	('United Kingdom', 'London'),
+
 	('Vatican City', 'Vatican City');
