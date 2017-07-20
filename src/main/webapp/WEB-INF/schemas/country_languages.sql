27c75196c47b6d61d3ce6ed66c2fa1282bc50c9f
DanielPiskorz <eel5426@gmail.com>
2017-07-20 18:36:21 +0200

    Splits sql schema.

---
+++ src/main/webapp/WEB-INF/schemas/country_languages.sql
@@ -0,0 +1,92 @@
+CREATE TABLE `country_languages`(
+	`name` varchar (32) NOT NULL,
+	`language` varchar (32) NOT NULL
+);
+
+INSERT INTO country_languages
+	(name, language)
+VALUES
+	('Albania', 'Albanian'),
+	('Andorra', 'Catalan'),
+	('Armenia', 'Armenian'),
+	('Austria', 'German'),
+	('Azerbaijan', 'Azerbaijani'),
+
+	('Belarus', 'Belarusian'),
+	('Belarus', 'Russian'),
+	('Belgium', 'German'),
+	('Belgium', 'French'),
+	('Belgium', 'Dutch'),
+	('Bosnia and Herzegovina', 'Bosnian'),
+	('Bosnia and Herzegovina', 'Croatian'),
+	('Bosnia and Herzegovina', 'Serbian'),
+	('Bulgaria', 'Bulgarian'),
+
+	('Croatia', 'Croatian'),
+	('Cyprus', 'Greek'),
+	('Cyprus', 'Turkish'),
+
+	('Czech Republic', 'Czech'),
+	('Czech Republic', 'Slovak'),
+ 
+	('Denmark', 'Danish'),
+ 
+	('Estonia', 'Estonian'),
+
+	('Finland', 'Finnish'),
+	('Finland', 'Swedish'),
+	('France', 'French'),
+
+	('Georgia',  'Georgian'),
+	('Germany', 'German'),
+	('Greece', 'Greek'),
+
+	('Hungary', 'Hungarian'),
+
+	('Iceland', 'Icelandic'),
+	('Ireland', 'Irish'),
+	('Ireland', 'English'),
+	('Italy', 'Italian'),
+
+	('Kosovo', 'Albanian'),
+	('Kosovo', 'Serbian'),
+
+	('Latvia', 'Latvian'),
+	('Liechtenstein', 'German'),
+	('Lithuania', 'Lithuanian'),
+	('Luxembourg', 'Luxembourgish'),
+	('Luxembourg', 'German'),
+	('Luxembourg', 'French'),
+
+	('Macedonia', 'Macedonian'),
+	('Malta', 'Maltese'),
+	('Malta', 'English'),
+	('Moldova',  'Romanian'),
+	('Monaco', 'French'),
+	('Montenegro', 'Montenegrin'),
+
+	('Netherlands', 'Dutch'),
+	('Norway', 'Norwegian'), 
+
+	('Poland', 'Polish'),
+	('Portugal', 'Portuguese'),
+
+	('Romania', 'Romanian'),
+	('Russia', 'Russian'),
+
+	('San Marino', 'Italian'),
+	('Serbia', 'Serbian'),
+	('Slovakia', 'Slovak'),
+	('Slovenia', 'Slovene'),
+	('Spain', 'Spanish'),
+	('Sweden', 'Swedish'),
+	('Switzerland', 'German'),
+	('Switzerland', 'French'),
+	('Switzerland', 'Italian'),
+
+	('Turkey', 'Turkish'),
+
+	('Ukraine', 'Ukrainian'),
+	('United Kingdom', 'English'),
+
+	('Vatican City', 'Italian');
